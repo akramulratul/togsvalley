068d6b1a5f6a13e5cfff7a9e4292a3d1d6667bbe
Chaion Saha <chaionsaha00@gmail.com>
2024-02-28 17:50:55 +0600

    item card has been updated

--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -1,12 +1,10 @@
 import {useState} from "react";
 import {useDispatch} from "react-redux";
-import {Box, Button, IconButton, Typography, useTheme} from "@mui/material";
-import AddIcon from "@mui/icons-material/Add";
-import RemoveIcon from "@mui/icons-material/Remove";
-import {shades} from "../theme";
-import {addToCart} from "../state";
-import {useNavigate} from "react-router-dom";
+import {useTheme} from "@mui/material";
+import {Link, useNavigate} from "react-router-dom";
 import useFetch from "../hooks/useFetch";
+import {addToCart} from "../state";
+import "./Item.scss";
 
 // Import Environment Variables
 const API_URL = process.env.REACT_APP_API_URL;
@@ -45,71 +43,105 @@ const Item = ({item, width}) => {
     }
 
     return (
-        <Box width={width}>
-            <Box
-                position="relative"
-                onMouseOver={() => setIsHovered(true)}
-                onMouseOut={() => setIsHovered(false)}
-            >
-                <img
-                    alt={item.name}
-                    width="300px"
-                    height="400px"
-                    src={imageUrl} // Use imageUrl here
-                    onClick={() => navigate(`/item/${item.id}`)}
-                    style={{cursor: "pointer"}}
-                />
-                <Box
-                    display={isHovered ? "block" : "none"}
-                    position="absolute"
-                    bottom="10%"
-                    left="0"
-                    width="100%"
-                    padding="0 5%"
-                >
-                    <Box display="flex" justifyContent="space-between">
-                        <Box
-                            display="flex"
-                            alignItems="center"
-                            backgroundColor={shades.neutral[100]}
-                            borderRadius="3px"
-                        >
-                            <IconButton onClick={() => setCount(Math.max(count - 1, 1))}>
-                                <RemoveIcon/>
-                            </IconButton>
-                            <Typography color={shades.primary[300]}>{count}</Typography>
-                            <IconButton onClick={() => setCount(count + 1)}>
-                                <AddIcon/>
-                            </IconButton>
-                        </Box>
-                        <Button
-                            onClick={() => {
-                                dispatch(addToCart({item: {...item, count}}));
-                            }}
-                            sx={{
-                                backgroundColor: shades.primary[300],
-                                color: "white",
-                                "&:hover": {
-                                    backgroundColor: "#7e7e7e", // Lighter green on hover
-                                },
-                            }}
-                        >
-                            Add to Cart
-                        </Button>
-                    </Box>
-                </Box>
-            </Box>
+        // <Box width={width}>
+        //     <Box
+        //         position="relative"
+        //         onMouseOver={() => setIsHovered(true)}
+        //         onMouseOut={() => setIsHovered(false)}
+        //     >
+        //         <img
+        //             alt={item.name}
+        //             width="300px"
+        //             height="400px"
+        //             src={imageUrl} // Use imageUrl here
+        //             onClick={() => navigate(`/item/${item.id}`)}
+        //             style={{cursor: "pointer"}}
+        //         />
+        //         <Box
+        //             display={isHovered ? "block" : "none"}
+        //             position="absolute"
+        //             bottom="10%"
+        //             left="0"
+        //             width="100%"
+        //             padding="0 5%"
+        //         >
+        //             <Box display="flex" justifyContent="space-between">
+        //                 <Box
+        //                     display="flex"
+        //                     alignItems="center"
+        //                     backgroundColor={shades.neutral[100]}
+        //                     borderRadius="3px"
+        //                 >
+        //                     <IconButton onClick={() => setCount(Math.max(count - 1, 1))}>
+        //                         <RemoveIcon/>
+        //                     </IconButton>
+        //                     <Typography color={shades.primary[300]}>{count}</Typography>
+        //                     <IconButton onClick={() => setCount(count + 1)}>
+        //                         <AddIcon/>
+        //                     </IconButton>
+        //                 </Box>
+        //                 <Button
+        //                     onClick={() => {
+        //                         dispatch(addToCart({item: {...item, count}}));
+        //                     }}
+        //                     sx={{
+        //                         backgroundColor: shades.primary[300],
+        //                         color: "white",
+        //                         "&:hover": {
+        //                             backgroundColor: "#7e7e7e", // Lighter green on hover
+        //                         },
+        //                     }}
+        //                 >
+        //                     Add to Cart
+        //                 </Button>
+        //             </Box>
+        //         </Box>
+        //     </Box>
+        //
+        //     <Box mt="3px">
+        //         <Typography variant="subtitle2" color={neutral.dark}>
+        //             {featured
+        //                 .replace(/([A-Z])/g, " $1")
+        //                 .replace(/^./, (str) => str.toUpperCase())}
+        //         </Typography>
+        //         <Typography className="text-base">{name}</Typography>
+        //         <Typography className="text-base" fontWeight="bold">৳ {price}</Typography>
+        //     </Box>
+        // </Box>
 
-            <Box mt="3px">
-                <Typography variant="subtitle2" color={neutral.dark}>
-                    {featured
-                        .replace(/([A-Z])/g, " $1")
-                        .replace(/^./, (str) => str.toUpperCase())}
-                </Typography>
-                <Typography className="text-base">{name}</Typography>
-                <Typography className="text-base" fontWeight="bold">৳ {price}</Typography>
-            </Box>
-        </Box>
+        <div>
+            <div className="relative">
+                <Link to={`/item/${item.id}`} className="add-to-cart-img">
+                    <img src={imageUrl} alt=""/>
+                </Link>
+                <div className="absolute w-full bottom-[4%] px-4 flex justify-between add-to-cart-hover">
+                    <div className="join text-white rounded-sm">
+                        <button onClick={() => setCount(Math.max(count - 1, 1))}
+                                className=' join-item btn btn-sm text-primary hover:bg-primary hover:text-white duration-150 border-transparent bg-white text-xl '>
+                            <i
+                                className='bi bi-dash'></i>
+                        </button>
+                        <p className='text-base  flex items-center join-item  px-2 bg-white text-primary '>{count}</p>
+                        <button onClick={() => setCount(count + 1)}
+                                className='border-none join-item bg-white text-primary hover:bg-primary hover:text-white duration-150 border-transparent   btn btn-sm text-xl '>
+                            <i
+                                className='bi bi-plus'></i>
+                        </button>
+                    </div>
+                    <button onClick={() => {
+                        dispatch(addToCart({item: {...item, count}}));
+                    }} className="btn btn-sm btn-primary text-white font-normal rounded-sm"><i
+                        className='bi bi-cart'></i>Add
+                        To Cart
+                    </button>
+                </div>
+
+            </div>
+            <div className="text-center text-lg mt-3">
+                <p>{name}</p>
+                <p>৳ {price}</p>
+            </div>
+        </div>
     );
 };
 
--- client/src/scenes/categoryProduct/CategoryProduct.jsx
+++ client/src/scenes/categoryProduct/CategoryProduct.jsx
@@ -8,7 +8,7 @@ const CategoryProduct = () => {
     const {data, loading, error} = useFetch(`/items?populate=image`);
     return (
         <div
-            className="text-2xl container lg:grid flex border-t border-gray-200 flex-col lg:grid-cols-4 grid-cols-1 pt-10 gap-10 items-center lg:px-0">
+            className="text-2xl container lg:grid flex border-t border-gray-200 flex-col lg:grid-cols-4 grid-cols-1 pt-10 gap-10 px-10 items-center lg:px-0">
             {
                 data && data.map((d, i) => <Item key={i} item={d}/>)
             }
